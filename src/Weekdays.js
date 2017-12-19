oilus19/react-day-picker-themeable

rename classnames

src/Weekdays.js
@@ -18,14 +18,17 @@ export default function Weekdays({
       locale,
     };
     const element = weekdayElement ?
-      React.cloneElement(weekdayElement, { ...elementProps, ...styling('weekday', i, locale) }) :
+      React.cloneElement(weekdayElement, {
+        ...elementProps,
+        ...styling('dayPickerWeekday', i, locale),
+      }) :
       React.createElement(weekdayComponent, elementProps);
     days.push(element);
   }
 
   return (
-    <div {...styling('weekdays', locale)} role="rowgroup">
-      <div {...styling('weekdaysRow', locale)} role="columnheader">
+    <div {...styling('dayPickerWeekdays', locale)} role="rowgroup">
+      <div {...styling('dayPickerWeekdaysRow', locale)} role="columnheader">
         {days}
       </div>
     </div>

alexkuz/react-day-picker

src/Weekdays.js
@@ -50,9 +50,9 @@ export default class Weekdays extends Component {
     }
 
     return (
-      <div {...styling('weekdays', locale)} role="rowgroup">
-        <div {...styling('weekdaysRow', locale)} role="row">
-          {showWeekNumbers && <div {...styling('weekday', locale)} />}
+      <div {...styling('dayPickerWeekdays', locale)} role="rowgroup">
+        <div {...styling('dayPickerWeekdaysRow', locale)} role="row">
+          {showWeekNumbers && <div {...styling('dayPickerWeekday', locale)} />}
           {days}
         </div>
       </div>