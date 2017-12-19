oilus19/react-day-picker-themeable

themeable react-day-picker

src/Weekdays.js
@@ -6,25 +6,26 @@ export default function Weekdays({
   localeUtils,
   weekdayComponent,
   weekdayElement,
+  styling,
 }) {
   const days = [];
   for (let i = 0; i < 7; i++) {
     const elementProps = {
       key: i,
-      className: 'DayPicker-Weekday',
+      styling,
       weekday: i,
       localeUtils,
       locale,
     };
     const element = weekdayElement ?
-      React.cloneElement(weekdayElement, elementProps) :
+      React.cloneElement(weekdayElement, { ...elementProps, ...styling('weekday', i, locale) }) :
       React.createElement(weekdayComponent, elementProps);
     days.push(element);
   }
 
   return (
-    <div className="DayPicker-Weekdays" role="rowgroup">
-      <div className="DayPicker-WeekdaysRow" role="columnheader">
+    <div {...styling('weekdays', locale)} role="rowgroup">
+      <div {...styling('weekdaysRow', locale)} role="columnheader">
         {days}
       </div>
     </div>
@@ -36,4 +37,5 @@ Weekdays.propTypes = {
   localeUtils: DayPickerPropTypes.localeUtils.isRequired,
   weekdayComponent: PropTypes.func,
   weekdayElement: PropTypes.element,
+  styling: PropTypes.func.isRequired,
 };

alexkuz/react-day-picker

src/Weekdays.js
@@ -3,11 +3,7 @@ import PropTypes from 'prop-types';
 
 export default class Weekdays extends Component {
   static propTypes = {
-    classNames: PropTypes.shape({
-      weekday: PropTypes.string.isRequired,
-      weekdays: PropTypes.string.isRequired,
-      weekdaysRow: PropTypes.string.isRequired,
-    }).isRequired,
+    styling: PropTypes.func.isRequired,
 
     firstDayOfWeek: PropTypes.number.isRequired,
     weekdaysLong: PropTypes.arrayOf(PropTypes.string),
@@ -26,7 +22,6 @@ export default class Weekdays extends Component {
   }
   render() {
     const {
-      classNames,
       firstDayOfWeek,
       showWeekNumbers,
       weekdaysLong,
@@ -34,18 +29,19 @@ export default class Weekdays extends Component {
       locale,
       localeUtils,
       weekdayElement,
+      styling,
     } = this.props;
     const days = [];
     for (let i = 0; i < 7; i += 1) {
       const weekday = (i + firstDayOfWeek) % 7;
       const elementProps = {
         key: i,
-        className: classNames.weekday,
         weekday,
         weekdaysLong,
         weekdaysShort,
         localeUtils,
         locale,
+        styling,
       };
       const element = React.isValidElement(weekdayElement)
         ? React.cloneElement(weekdayElement, elementProps)
@@ -54,9 +50,9 @@ export default class Weekdays extends Component {
     }
 
     return (
-      <div className={classNames.weekdays} role="rowgroup">
-        <div className={classNames.weekdaysRow} role="row">
-          {showWeekNumbers && <div className={classNames.weekday} />}
+      <div {...styling('weekdays', locale)} role="rowgroup">
+        <div {...styling('weekdaysRow', locale)} role="row">
+          {showWeekNumbers && <div {...styling('weekday', locale)} />}
           {days}
         </div>
       </div>